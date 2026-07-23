1316323c9707705f007fd1fb269b081a819537ca
Divya Ratna Gautam <146772314+vayugautam@users.noreply.github.com>
2026-07-23 22:16:54 +0530

    Time: 0 ms (100%), Space: 125.4 MB (26.53%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,14 +1,8 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-
-        if (n <= 2) return n;
-
-        int mask = 0;
-        for (int num : nums) {
-            mask |= num;
-        }
-
-        return mask + 1;
+        if(n < 3) return n;
+        int nearest = (int)(Math.log(n)/Math.log(2));
+        return 1 << (nearest + 1);
     }
 }
