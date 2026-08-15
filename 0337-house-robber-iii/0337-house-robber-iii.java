9065fb1775716c6511752681a1f653a0b32f6bf6
Divya Ratna Gautam <146772314+vayugautam@users.noreply.github.com>
2026-08-16 00:52:46 +0530

    Time: 0 ms (100%), Space: 46.5 MB (73.7%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -3,12 +3,15 @@ class Solution {
         int[] ans = func(root);
         return Math.max(ans[0],ans[1]);
     }
+    // 0 = value ko leliya hai 
+    // 1 = value ko nahi liya hai
     int[] func(TreeNode root){
         if(root==null) {
             return new int[]{0,0};
         }
         int[] left = func(root.left);
         int[] right = func(root.right);
+
 
         int rob = root.val + left[1]+right[1];
         int notRob = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
