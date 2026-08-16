a73d10c5426649aeb036a65f7d6b5112cfd21f70
Divya Ratna Gautam <146772314+vayugautam@users.noreply.github.com>
2026-08-16 17:25:34 +0530

    Time: 4 ms (95.4%), Space: 68.8 MB (88.77%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.java
+++ 0565-array-nesting/0565-array-nesting.java
@@ -1,17 +1,18 @@
 class Solution {
     public int arrayNesting(int[] nums) {
         int n = nums.length;
-        HashSet<Integer> hs = new HashSet<>();
-        int ans = Integer.MIN_VALUE;
-        for(int i = 0 ; i < n ; i++){
-            int j = i;
-            int count = 0;
-            while(!hs.contains(nums[j])){
-                hs.add(nums[j]);
-                j = nums[j];
+        boolean[] visited = new boolean[n];
+        int ans=0;
+        for(int k=0;k<n;k++){
+            if(visited[k]) continue;
+            int current=k;
+            int count=0;
+            while(!visited[current]){
+                visited[current]=true;
                 count++;
+                current=nums[current];
             }
-            ans = Math.max(ans , count);
+            ans=Math.max(ans,count);
         }
         return ans;
     }
