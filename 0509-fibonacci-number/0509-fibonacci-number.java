a9446450202880920d4d4e2d4165f73d6cc15f75
Divya Ratna Gautam <146772314+vayugautam@users.noreply.github.com>
2026-08-29 15:33:54 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,14 +2,12 @@ class Solution {
     public int fib(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return func(n,dp);
-    }
-    int func(int n,int[] dp){
-        if(n<=1) {
-            dp[n]=n;
+        if(n<=1) return n;
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=-1) return dp[n];
-        
-        return dp[n]=func(n-1,dp)+func(n-2,dp);
+        return dp[n];
     }
 }
